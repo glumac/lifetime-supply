4e3c2fc231eea9528fadf9970999e07832759656
glumac <mglumac@gmail.com>
2018-10-27 16:23:29 -0400

    solution

--- js/project.js
+++ js/project.js
@@ -1,7 +1,17 @@
-// assign click event handler with jQuery
+// assign click event handler
+document.querySelector('#click-me').onclick = calculateNumDrinks;
 
-function calculateNumDrinks() {
-  // assign variables
-  // calculate total
-  // output results
+function calculateNumDrinks () {
+	// assign variables
+	var currentAge = document.querySelector('#age').value;
+	var maxAge = document.querySelector('#max-age').value;
+	var favDrink = document.querySelector('#item').value;
+	var numPerDay = document.querySelector('#num-per-day').value;
+
+	// calculate total
+	var total = (maxAge - currentAge) * 365 * numPerDay;
+
+	// output results
+	document.querySelector('#solution').innerHTML = total;
+	document.querySelector('#drink').innerHTML = favDrink;
 }
